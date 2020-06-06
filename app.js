e2ce119d394410a8787397416c4eb8166fc82dbd
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-06-07 01:09:41 +0430

    Done with the map section scripts

--- app.js
+++ app.js
@@ -34,6 +34,39 @@ $("#select").change(() => {
 
     // Flag section
     $("#flag").attr("src", country[0].flag);
-    
+
+    // Wheater section
+
+    // Map section
+    $("#map").empty();
+    const app = new Mapp({
+      element: "#map",
+      presets: {
+        latlng: {
+          lat: country[0].latlng[0],
+          lng: country[0].latlng[1],
+        },
+        zoom: 3,
+      },
+      apiKey:
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjlmMWJmOGM1MzQxMTgwYTAxMmNiMDQ4ZTUzMWZiODZlY2Y5NTQwZWU1NjBhMWFjNTViYmVlNTdhOWViOWJhNjNkZmZlODk4NTU4ZTM4MmVhIn0.eyJhdWQiOiI5NTI4IiwianRpIjoiOWYxYmY4YzUzNDExODBhMDEyY2IwNDhlNTMxZmI4NmVjZjk1NDBlZTU2MGExYWM1NWJiZWU1N2E5ZWI5YmE2M2RmZmU4OTg1NThlMzgyZWEiLCJpYXQiOjE1OTE0NDQyMjEsIm5iZiI6MTU5MTQ0NDIyMSwiZXhwIjoxNTk0MDM2MjIxLCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.EQ2TwYQ3smTxplc7YsLx4Mw5nWsW57rAOsmmiSaiJh4nFdwDtPXwkuF_YAAHMs0dkHm6OxqW4iYTL6-9ilMwUvgJ_mXAI13YV9MPYqW_VmRAuNeJxUffVMJ0peqbHE0-AbYhVAcGdgAlY0ULZcSFRaDg0HpB0MyWK51d-Xr604oK4I1_nXvhh0_XYHWGx-dc2IlV7Z4w9fsyeBK4POAFYdulquAYFkmPrnMwbWybEXrcLnU7vShp1MKz5ieFMQZ5jaKWXt1eV148COCYuiJhSx0tSKdV5PSEGn8T1J9DGonFl641pMJNzRA45C08L58ktQ2eLOfYfq2aB7iAGqNsnA",
+    });
+    app.addLayers();
+    app.addMarker({
+      name: "basic-marker",
+      latlng: {
+        lat: country[0].latlng[0],
+        lng: country[0].latlng[1],
+      },
+      popup: {
+        title: {
+          html: "Basic Marker Title",
+        },
+        description: {
+          html: "Basic marker description",
+        },
+        open: true,
+      },
+    });
   });
 });
